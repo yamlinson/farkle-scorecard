64767d97d874f62539484e9ef2a1d2c205a0a514
Ryan Tomlinson <rtomlinson118@gmail.com>
2023-08-20 22:26:47 -0600

    Init Vue frontend

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="main">
+    <div class="card">
+      <h2>Enter Room Code to Join a Game</h2>
+    </div>
+    <div class="card">
+      <h2>Sign In to Manage Games</h2>
+    </div>
+  </div>
+</template>
