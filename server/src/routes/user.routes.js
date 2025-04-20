fd791f7999657b6bee7c746874eb283ad6cc1ee0
yamlinson <95899054+yamlinson@users.noreply.github.com>
2025-04-19 20:52:41 -0600

    Reset project in preparation for Rails

--- server/src/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { loginUser } = require("../controllers/user.controller");
-
-router.route("/login").post(loginUser);
-
-module.exports = router;
